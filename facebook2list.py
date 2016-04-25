4bdbf124c63280b82b666ec876f33488a3a7bb22
Yotam Alon <Yotamalon@gmail.com>
2016-04-25 12:35:42 -0700

    Basic Structure completed

--- facebook2list.py
+++ facebook2list.py
@@ -1,10 +1,28 @@
+#Kivy Imports
 import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 
+#Regular Imports
+from os.path import isfile
+
+
 class MainWindow(GridLayout):
     def __init__(self):
         super(MainWindow, self).__init__(self)
+
+	def parse(self):
+		text = self.ids.post.text
+	    pass #when I see how the post looks I will continue this, should return a string
+
+	def update(self):
+		if isfile("fb_posts.txt"):
+			f = open("fb_posts.txt", 'r+')
+	    else:
+            f = open("fb_posts.txt", "w+")
+
+        f.write(self.parse())
+        f.close()
 
 
 class Facebook2listApp(App):
